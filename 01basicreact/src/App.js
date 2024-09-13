c12583c4c5754ee13c40f105107225578dd42b6c
BhautikVekariya21 <bhautikvekariya1123@gmail.com>
2024-09-13 14:21:39 +0530

    react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>welcome react || bhautik vekariya</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>vite+react || bv</h1>
+    </>
+  )
+}
+
+export default App
