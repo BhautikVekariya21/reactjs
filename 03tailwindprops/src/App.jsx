0bdb77edb4e5ee6663751826566ff20dbcd780b3
BhautikVekariya21 <bhautikvekariya1123@gmail.com>
2024-09-13 22:29:22 +0530

    props with tailwind

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { ProductOverviewOne } from './assets/ProductOverviewOne';
+import { ProductOverviewOne } from './ProductOverviewOne';
 import './App.css'
 import { ProductThree } from './ProductThree';
 
