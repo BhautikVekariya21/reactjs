c1b6a114eed59739b55a224edaa0b70923c5cfa5
BhautikVekariya21 <bhautikvekariya1123@gmail.com>
2024-09-13 22:25:54 +0530

    use props with tailwind

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { ProductOverviewOne } from './assets/ProductOverviewOne';
+import './App.css'
+import { ProductThree } from './ProductThree';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+    <ProductOverviewOne username="Nike" thanks="Thank You For Shopping With Us"/>
+    <ProductThree one="Nike Air Max 90"/>
+    </>
+  )
+}
+
+export default App
